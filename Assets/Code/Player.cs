24a38507e99cafb69fa79f42a962bac97f8b0d08
Andrew Friedman <andrewfriedman01@gmail.com>
2018-05-31 02:12:51 -0400

    Added App
    This is the first version of Piper_Bird that GitHub has ever seen!

---
+++ Assets/Code/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public GameObject Camera;
+	public GameObject Canvas;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Canvas.GetComponent<Gamemaster>().Game_State == "Game") {
+			this.gameObject.transform.position = new Vector2(this.gameObject.transform.position.x + .025f, this.gameObject.transform.position.y);
+			if (this.gameObject.transform.position.y >= 5.5 || this.gameObject.transform.position.y <= -5.5) {
+				Canvas.GetComponent<Gamemaster>().Game_State = "End";
+			}
+		}
+
+		Camera.transform.position = new Vector3(this.gameObject.transform.position.x, 0, -10);
+
+		if (Input.GetMouseButtonDown(0)) {
+			this.gameObject.GetComponent<Rigidbody2D>().AddForce(3f * new Vector2(0, 2f), ForceMode2D.Impulse);
+		}
+	}
+}
